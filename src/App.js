a306fb28d53850c50bf890241b59df6873b5f7dd
Nimmy The Great <Nimrod@reshef.com>
2020-07-28 21:29:17 +0300

    final

--- src/App.js
+++ src/App.js
@@ -3,39 +3,85 @@ import { fetchData, } from './utils/fetch.api'
 
 function App() {
 
-    const [response, setResponse, ] = React.useState({ 
-        error: null, 
-        loading: true, 
-        data: null 
+    const [response, setResponse,] = React.useState({
+        error: null,
+        loading: true,
+        data: null
     })
 
     React.useEffect(() => {
 
         fetchData('http://localhost:8080/article/all')
-            .then(data => setResponse({...response, data, loading: false, }))
-            .catch(error => setResponse({...response, error, loading: false}))
+            .then(data => setResponse({ ...response, data, loading: false, }))
+            .catch(error => setResponse({ ...response, error, loading: false }))
     }, [])
 
     const { data, loading, error, } = response
 
-    if(error) {
+    if (error) {
         console.log('!! ', error)
         return null;
     }
 
-    if(loading) {
+    if (loading) {
         return (
             <h1>Loading</h1>
         )
     }
 
-    if(data) {
-        console.log('!!! ', data)
-    }
-    
+    const { content: articles, } = data
+
+
     return (
-        <div>
-            
+        <div style={{
+            margin: '1rem',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            {articles.map(({ author, header, paragraphs }) => (
+                <div
+                    key={header}
+                    style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        borderRadius: '7px',
+                        boxShadow: '0 3px 4px #ccc',
+                        minWidth: '80%',
+                        maxWidth: '80%',
+                        margin: '1rem auto',
+                    }}
+                >
+                    <div style={{
+                        padding: '1rem',
+                        // borderRadius: '7px',
+                        borderRight: '3px solid #ccc',
+                        backgroundColor: '#d9d9d9'
+                    }}>
+                        <h2 style={{ textAlign: 'center' }}>
+                            {`${author.firstName} ${author.lastName}`}
+                        </h2>
+                        <img
+                            src={`${author.image}`}
+                            style={{
+                                maxWidth: '10rem',
+                                maxHeight: '10rem',
+                                borderRadius: '7px',
+                                boxShadow: '0 5px 7px #ccc'
+                            }}
+                        />
+                    </div>
+
+                    <div style={{ padding: '1rem', }}>
+                        <h1>{header}</h1>
+                        {paragraphs.map(({ content, }) => (
+                            <p key={content}>
+                                {content}
+                            </p>
+                        ))}
+                    </div>
+
+                </div>
+            ))}
         </div>
     );
 }
