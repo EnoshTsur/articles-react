a87d9215371081809d52b661fd2819084b0f64b2
Nimmy The Great <Nimrod@reshef.com>
2020-07-28 20:36:31 +0300

    react-articles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { fetchData, } from './utils/fetch.api'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [response, setResponse, ] = React.useState({ 
+        error: null, 
+        loading: true, 
+        data: null 
+    })
+
+    React.useEffect(() => {
+
+        fetchData('http://localhost:8080/article/all')
+            .then(data => setResponse({...response, data, loading: false, }))
+            .catch(error => setResponse({...response, error, loading: false}))
+    }, [])
+
+    const { data, loading, error, } = response
+
+    if(error) {
+        console.log('!! ', error)
+        return null;
+    }
+
+    if(loading) {
+        return (
+            <h1>Loading</h1>
+        )
+    }
+
+    if(data) {
+        console.log('!!! ', data)
+    }
+    
+    return (
+        <div>
+            
+        </div>
+    );
 }
 
 export default App;
